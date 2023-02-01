c81dde07b71a1be6c08e238f39d6baa7bb35385c
Александр <alextrexen@gmail.com>
2023-02-01 14:12:09 +0300

    Оптимизация кодовой базы

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,7 +1,14 @@
 const page = document.querySelector('body');
 const menu = page.querySelector('.main-nav__menu-list');
+const menuItem = menu.querySelectorAll('.main-nav__link');
 const menuBtn = page.querySelector('.main-nav__menu-button');
 
 menuBtn.onclick = function () {
   menu.classList.toggle('main-nav__menu-list--open')
 };
+
+menuItem.forEach(a => {
+  a.onclick = function () {
+    menu.classList.remove('main-nav__menu-list--open')
+  };
+})

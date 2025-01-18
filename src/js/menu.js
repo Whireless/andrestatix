f1282aac61d6b6a630063d760d96a66283df9caf
Александр <alextrexen@gmail.com>
2025-01-18 16:35:41 +0300

    Планшетная версия v1.0

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,16 @@
 const page = document.querySelector('html');
-const body = page.querySelector('body');
-const menu = body.querySelector('.main-nav__list');
-const open = body.querySelector('.main-nav__menu-button');
+const menu = page.querySelector('.main-nav');
+const open = menu.querySelector('.main-nav__menu-button');
 const allLinks = menu.querySelectorAll('.main-nav__link');
 
 // Открытие/закрытие мобильного меню
 
-open.addEventListener('click', () => menu.classList.toggle('main-nav__list--open'));
+open.addEventListener('click', () => menu.classList.toggle('main-nav--open'));
 
 // Закрытие меню при нажатии ссылки
 
 allLinks.forEach((link) => {
   link.addEventListener('click', () => {
-    menu.classList.remove('main-nav__list--open');
+    menu.classList.remove('main-nav--open');
   });
 });
